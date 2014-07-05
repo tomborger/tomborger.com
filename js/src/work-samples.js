55baeb9b0b8da27ad083854dd78b7971fcde0bc3
Tom Borger <thomas.borger@gmail.com>
2014-07-05 12:53:53 -0500

    Layout on window load

--- js/src/work-samples.js
+++ js/src/work-samples.js
@@ -1,36 +1,36 @@
 /**
  * Manage work sample interaction
  */
-$(function(){
-
+$(document).ready( function(){
 	$('.work-samples').isotope({
 		itemSelector:'.work-sample',
 		layoutMode:'masonry'
 	});
+});
 
-	$('.work-sample-title, .work-sample-image').on('click', function(){
-		var $ws = $(this).closest('.work-sample');
-		if( ! $ws.hasClass('expanded') ) {
-			ga( 'send', 'event', 'interaction', 'expand work sample', $ws.find( '.work-sample-title' ).text(), {'nonInteraction': 1} );
-			$ws.addClass('expanded');
-			$('.work-samples').isotope('layout');
-			e.stopPropagation();
-		}
-	});
+$(window).on( 'load', function(){
+	$('.work-samples').isotope('layout');
+});
 
-	$('.work-sample-menu-close, .work-sample-image').on('click', function(e){
-		var $ws = $(this).closest('.work-sample');
-		if( $ws.hasClass('expanded') ){
-			$ws.removeClass('expanded');
-			e.stopPropagation();
-			$('.work-samples').isotope('layout');		
-		}
-	});
+$('.work-sample-title, .work-sample-image').on('click', function(){
+	var $ws = $(this).closest('.work-sample');
+	if( ! $ws.hasClass('expanded') ) {
+		ga( 'send', 'event', 'interaction', 'expand work sample', $ws.find( '.work-sample-title' ).text(), {'nonInteraction': 1} );
+		$ws.addClass('expanded');
+		$('.work-samples').isotope('layout');
+		e.stopPropagation();
+	}
+});
 
-	$('.work-sample-menu-visit').on('click', function(){
-		ga( 'send', 'event', 'links', 'click', $(this).attr('href'), {'nonInteraction': 1} );	
-	});
+$('.work-sample-menu-close, .work-sample-image').on('click', function(e){
+	var $ws = $(this).closest('.work-sample');
+	if( $ws.hasClass('expanded') ){
+		$ws.removeClass('expanded');
+		e.stopPropagation();
+		$('.work-samples').isotope('layout');		
+	}
+});
 
-
-
+$('.work-sample-menu-visit').on('click', function(){
+	ga( 'send', 'event', 'links', 'click', $(this).attr('href'), {'nonInteraction': 1} );	
 });
